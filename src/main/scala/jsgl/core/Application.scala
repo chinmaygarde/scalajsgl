f0a2e375bd31f88041c03681fd52085660b534b7
Chinmay Garde <chinmay@apportable.com>
2013-12-02 01:22:09 -0800

    Basic application stubbed

---
+++ src/main/scala/jsgl/core/Application.scala
@@ -0,0 +1,27 @@
+
+
+abstract class Application(var root: Node) {
+
+  protected def heartbeat() = {
+    update()
+    layout()
+    display()
+    render()
+  }
+
+  protected def layout() = {
+
+  }
+
+  protected  def display() {
+
+  }
+
+  protected def render() {
+
+  }
+
+  protected def update() {
+
+  }
+}
